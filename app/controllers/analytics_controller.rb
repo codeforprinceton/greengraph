94709fcea85982bc6fbd23ce64983a7507132c29
Slacker87 <ccnp_pro@yahoo.com>
2015-05-26 19:56:27 +0000

    lots of data manipulation fixes, and a new graph

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,8 +1,20 @@
 class AnalyticsController < ApplicationController
   def index
       #get all gas readings for each class
-      @commercialgas = Reading.where('business_class = ? and gas_billed IS NOT NULL', "Commercial").pluck(:gas_billed).sum
-      @industrialgas = Reading.where('business_class = ? and gas_billed IS NOT NULL', "Industrial").pluck(:gas_billed).sum
-      @residentialgas = Reading.where('business_class = ? and gas_billed IS NOT NULL', "Residential").pluck(:gas_billed).sum
+      comgasboro = Reading.where('business_class = ? and gas_billed IS NOT NULL and city_code = ?', "Commercial", "PRINCETON BORO")
+      comgastwp = Reading.where('business_class = ? and gas_billed IS NOT NULL and city_code = ?', "Commercial", "PRINCETON TWP")
+      indgasboro = Reading.where('business_class = ? and gas_billed IS NOT NULL and city_code = ?', "Industrial", "PRINCETON BORO")
+      indgastwp = Reading.where('business_class = ? and gas_billed IS NOT NULL and city_code = ?', "Industrial", "PRINCETON TWP")
+      resgasboro = Reading.where('business_class = ? and gas_billed IS NOT NULL and city_code = ?', "Residential", "PRINCETON BORO")
+      resgastwp = Reading.where('business_class = ? and gas_billed IS NOT NULL and city_code = ?', "Residential", "PRINCETON TWP")
+      @commercialgas = comgasboro.pluck(:gas_billed).sum + comgastwp.pluck(:gas_billed).sum
+      @industrialgas = indgasboro.pluck(:gas_billed).sum + indgastwp.pluck(:gas_billed).sum
+      @residentialgas = resgasboro.pluck(:gas_billed).sum + resgastwp.pluck(:gas_billed).sum
+      #break down gas readings per reading per class
+      comgas2009 = comgasboro.where(read_date: DateTime.new(2009)..(DateTime.new(2010) - 1.month))
+      @commercialgasbreakread = comgas2009.pluck(:read_date).map{|readdate| readdate.strftime("%B, %Y").gsub(/,/, '')}
+      @commercialgasbreakbilled = comgas2009.pluck(:gas_billed)
+      @industrialgasbreak = Reading.where('business_class = ? and gas_billed IS NOT NULL', "Industrial").pluck(:read_date, :gas_billed)
+      @residentialgasbreak = Reading.where('business_class = ? and gas_billed IS NOT NULL', "Residential").pluck(:read_date, :gas_billed)
   end
 end
