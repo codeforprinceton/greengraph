ef25537f31d91016fc81a9658d632b6ce241dea5
Slacker87 <ccnp_pro@yahoo.com>
2015-05-26 20:39:42 +0000

    DRY up the code a bit

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,15 +1,15 @@
 class AnalyticsController < ApplicationController
   def index
-      #get all gas readings for each class -- MOVE THESE TO MODEL
-      comgasboro = Reading.where('business_class = ? and gas_billed IS NOT NULL and city_code = ?', "Commercial", "PRINCETON BORO")
-      comgastwp = Reading.where('business_class = ? and gas_billed IS NOT NULL and city_code = ?', "Commercial", "PRINCETON TWP")
-      indgasboro = Reading.where('business_class = ? and gas_billed IS NOT NULL and city_code = ?', "Industrial", "PRINCETON BORO")
-      indgastwp = Reading.where('business_class = ? and gas_billed IS NOT NULL and city_code = ?', "Industrial", "PRINCETON TWP")
-      resgasboro = Reading.where('business_class = ? and gas_billed IS NOT NULL and city_code = ?', "Residential", "PRINCETON BORO")
-      resgastwp = Reading.where('business_class = ? and gas_billed IS NOT NULL and city_code = ?', "Residential", "PRINCETON TWP")
-      @commercialgas = comgasboro.pluck(:gas_billed).sum + comgastwp.pluck(:gas_billed).sum
-      @industrialgas = indgasboro.pluck(:gas_billed).sum + indgastwp.pluck(:gas_billed).sum
-      @residentialgas = resgasboro.pluck(:gas_billed).sum + resgastwp.pluck(:gas_billed).sum
+      #get all gas readings for each class
+      comgasboro = Reading.getbill("gas", "Commercial", "PRINCETON BORO")
+      comgastwp = Reading.getbill("gas", "Commercial", "PRINCETON TWP")
+      indgasboro = Reading.getbill("gas", "Industrial", "PRINCETON BORO")
+      indgastwp = Reading.getbill("gas", "Industrial", "PRINCETON TWP")
+      resgasboro = Reading.getbill("gas", "Residential", "PRINCETON BORO")
+      resgastwp = Reading.getbill("gas", "Residential", "PRINCETON TWP")
+      @commercialgas = Reading.billsum("gas", comgasboro, comgastwp)
+      @industrialgas = Reading.billsum("gas", indgasboro, indgastwp)
+      @residentialgas = Reading.billsum("gas", resgasboro, resgastwp)
       #break down gas readings per reading per class -- MOVE THESE TO MODEL
       comgas2009 = comgasboro.where(read_date: DateTime.new(2009)..(DateTime.new(2010) - 1.month))
       indgas2009 = indgasboro.where(read_date: DateTime.new(2009)..(DateTime.new(2010) - 1.month))
--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,2 +1,19 @@
 class Reading < ActiveRecord::Base
+    def self.getbill(type, sector, location)
+        if type == "gas"
+            result = self.where('business_class = ? and gas_billed IS NOT NULL and city_code = ?', sector, location)
+        elsif type == "electric"
+            result = self.where('business_class = ? and electric_billed IS NOT NULL and city_code = ?', sector, location)
+        end
+        return result
+    end
+    
+    def self.billsum(type, twp, boro)
+        if type == "gas"
+            result = twp.pluck(:gas_billed).sum + boro.pluck(:gas_billed).sum
+        elsif type == "electric"
+            result = twp.pluck(:electric_billed).sum + boro.pluck(:electric_billed).sum
+        end
+        return result
+    end
 end
