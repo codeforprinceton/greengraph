676939fceebcf5955cc5d7a02b3886128c70ec30
Slacker87 <ccnp_pro@yahoo.com>
2015-05-26 18:59:12 +0000

    start putting data to graphs

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,4 +1,8 @@
 class AnalyticsController < ApplicationController
   def index
+      #get all gas readings for each class
+      @commercialgas = Reading.where('business_class = ? and gas_billed IS NOT NULL', "Commercial").pluck(:gas_billed).sum
+      @industrialgas = Reading.where('business_class = ? and gas_billed IS NOT NULL', "Industrial").pluck(:gas_billed).sum
+      @residentialgas = Reading.where('business_class = ? and gas_billed IS NOT NULL', "Residential").pluck(:gas_billed).sum
   end
 end
