72f1bfa4061f20848672533ec033b5123a1b0104
Slacker87 <ccnp_pro@yahoo.com>
2015-05-27 05:37:30 +0000

    start styling homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  match 'about', to: 'home#about', via: 'get', as: 'about'
   match 'analytics', to: 'analytics#index', via: 'get', as: 'analytics'
   match 'api', to: 'api#index', via: 'get', as: 'api'
   match 'api/raw', to: 'api#raw', via: 'get', as: 'raw_api'
