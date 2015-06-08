ca57d25b6e1ff1cfa4f479f4c10ba2f2738d77ed
Slacker87 <ccnp_pro@yahoo.com>
2015-06-08 01:56:16 +0000

    fix an asset compile error in production

--- greengraph/config/application.rb
+++ greengraph/config/application.rb
@@ -22,5 +22,6 @@ module PtonEnergy
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
