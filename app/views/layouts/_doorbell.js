79d04703e7b5b2e85bb6805c66f57632085d8af5
Slacker87 <ccnp_pro@yahoo.com>
2015-11-24 03:05:40 +0000

    add a feedback button

---
+++ app/views/layouts/_doorbell.js
@@ -0,0 +1,7 @@
+
+    window.doorbellOptions = {
+        appKey: 'vutcclob49p53dZnQfsXqWSvJ55bGoIuD4UdZMGDhOSIq3SgZ701OrByzFGJ2oCv'
+    };
+    (function(d, t) {
+        var g = d.createElement(t);g.id = 'doorbellScript';g.type = 'text/javascript';g.async = true;g.src = 'https://embed.doorbell.io/button/2637?t='+(new Date().getTime());(d.getElementsByTagName('head')[0]||d.getElementsByTagName('body')[0]).appendChild(g);
+    }(document, 'script'));
